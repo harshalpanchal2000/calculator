216d4d2bc1503fa3dd970151224c84063ab31e84
Harshal Panchal <78840044+harshalpanchal2000@users.noreply.github.com>
2024-04-13 10:59:20 -0400

    Update Setup.py

--- Setup.py
+++ Setup.py
@@ -1,1 +1,22 @@
+from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="sumfunction",
+    version="0.1",
+    author="Harshal Panchal",
+    author_email="contact@itsmeharshal.com",
+    description="A simple function to calculate the sum of two numbers.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/your_username/sumfunction",
+    py_modules=["sumfunction"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
